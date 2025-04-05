2474c5297875e9dcede4249b93bc4523eea5177b
anvarDev <amujinov@gmail.com>
2025-04-06 00:52:03 +0500

    update

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "7928128631:AAH5tdOKkdjF_3J7Z7quULd6oLUG-hrbmyE"
+BOT_TOKEN = "7928128631:AAH0Ugml12nu93cGwHrdAGkdyor5llfwCgw"
 ADMINS = [6369838846,7872028789]  # Добавьте свой ID
 WEATHER_API_KEY = '8aab62712df12fec699d40dc4c82467d'
